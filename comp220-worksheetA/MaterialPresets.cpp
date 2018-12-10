1c2f8ea8df1c572a56810dc0cd4b5b3f1a6d9671
TMGilchrist <telcontar@hotmail.co.uk>
2018-12-10 10:07:42 +0000

    Changed the near clip plane to fix graphical glitches. (thanks for the tip Louis!)

--- comp220-worksheetA/MaterialPresets.cpp
+++ comp220-worksheetA/MaterialPresets.cpp
@@ -26,5 +26,5 @@ void MaterialPresets::Init()
 	metal.Init(glm::vec4(1.0f, 1.0f, 1.0f, 0.0f), glm::vec4(0.8f, 0.8f, 0.8f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 25);
 
 	stone = Material();
-	stone.Init(glm::vec4(1.0f, 1.0f, 1.0f, 0.0f), glm::vec4(0.8f, 0.8f, 0.8f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 2);
+	stone.Init(glm::vec4(1.0f, 1.0f, 1.0f, 0.0f), glm::vec4(0.8f, 0.8f, 0.8f, 0.0f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), 20);
 }
