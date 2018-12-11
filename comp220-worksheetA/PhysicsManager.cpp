0341d544896efbb70174c7d887cc2804882ac8f0
Mango <TG190896@falmouth.co.uk>
2018-12-11 01:07:22 +0000

    Added a landscape mesh with fewer vertices (60k down from 360k) Still takes 5 minutes to load and crashes with vectorSubscript out of range.

--- comp220-worksheetA/PhysicsManager.cpp
+++ comp220-worksheetA/PhysicsManager.cpp
@@ -74,7 +74,7 @@ btCollisionShape * PhysicsManager::CreateConvexCollider(GameObject * object, Mes
 
 	btConvexHullShape* collider = new btConvexHullShape();
 
-	for (int i = 0; i < 10000; i++)
+	for (int i = 0; i < 100000; i++)
 	{
 		//collider->addPoint(pointData[i]);
 	}
